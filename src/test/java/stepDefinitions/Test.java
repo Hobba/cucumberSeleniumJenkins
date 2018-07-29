f9648a336aa7c93a752d073f0ea32836929ba184
Hobba91 <hobba11@msn.com>
2018-07-29 14:09:58 +0200

    initial commit

---
+++ src/test/java/stepDefinitions/Test.java
@@ -0,0 +1,30 @@
+package stepDefinitions;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Test {
+	
+	@Given("user is on homepage")
+	public void user_is_on_homepage() {
+	    
+	}
+
+	@When("user navigates to Login Page")
+	public void user_navigates_to_Login_Page() {
+	    
+	}
+
+	@And("user enters username and Password")
+	public void user_enters_username_and_Password() {
+	    
+	}
+
+	@Then("success message is displayed")
+	public void success_message_is_displayed() {
+	
+	}
+
+}
